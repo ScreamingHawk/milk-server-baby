8bea5f0f94de2bea0d54396dbfdf0b25a181ee10
Michael Standen <screaminghawk@gmail.com>
2015-09-24 11:04:16 +1200

    Removed blacklisting. Doesn't seem to be working

--- babyUpvote.js
+++ babyUpvote.js
@@ -7,32 +7,21 @@ exports.handler = function(event, context) {
     var bname = event.name.replace(/\W/g, '');
     bname = bname.charAt(0).toUpperCase() + bname.slice(1);
     
-    var n = dynamodb.getItem({
+    dynamodb.updateItem({
         "TableName": "baby-names",
-        "Key" : {
-            name : bname
-        }
-    });
-    
-    if (n.votes >= 0){
-        dynamodb.updateItem({
-            "TableName": "baby-names",
-            "Key" : {
-                name : bname
-            },
-            "UpdateExpression" : "SET votes = votes + :a",
-            "ExpressionAttributeValues" : {
-                ":a" : 1
-            }
-        }, function(err, data) {
-            if (err) {
-                context.done('error putting item into dynamodb failed: '+err);
-            } else {
-                console.log('success: '+JSON.stringify(data, null, '  '));
-                context.done();
-            }
-        });
-    } else {
-        console.log('blocking upvote on blacklisted name: '+bname);
-    }
+		"Key" : {
+			name : bname
+		},
+		"UpdateExpression" : "SET votes = votes + :a",
+		"ExpressionAttributeValues" : {
+			":a" : 1
+		}
+	}, function(err, data) {
+		if (err) {
+			context.done('error putting item into dynamodb failed: '+err);
+		} else {
+			console.log('success: '+JSON.stringify(data, null, '  '));
+			context.done();
+		}
+	});
 };
